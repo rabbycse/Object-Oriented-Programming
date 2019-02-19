2391d76f9d6f6ad3e9ed83452717acad9b8ca855
@rabby <37344605+rabbycse@users.noreply.github.com>
2019-02-19 12:18:08 +0600

    Composition

---
+++ Composition/Composition/Job.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composition
+{
+    class Job
+    {
+        private string name;
+        private double salary;
+
+        public Job()
+        {
+            Person = new Person(this);
+        }
+
+        public Job(string name, double salary)
+        {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        private string getName 
+        {
+            get
+            {
+                return name;
+            }
+
+        }
+
+        public void HowIsTheJob(bool good)
+        {
+            if(good)
+            {
+                Person.IsSuccess = true;
+            }
+            else
+            {
+                Person.IsSuccess = false;
+            }
+        }      
+    }
+}
---
+++ Composition/Composition/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composition
+{
+    class Person
+    {
+        private Job job;
+        public Boolean IsSuccess = false;
+
+        public Person(Job job)
+        {
+            this.job = job;
+        }
+
+        public bool IsSuccess
+        {
+            get { return IsSuccess; }
+            set { IsSuccess = value;
+
+                if(value)
+                {
+                    this.job.salary++;
+                }
+                else
+                {
+                    this.job.salary--;
+                }
+            }
+
+    }
+}
